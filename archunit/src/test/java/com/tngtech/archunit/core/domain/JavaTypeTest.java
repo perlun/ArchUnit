b4e81e04b1526bc6dfc43e01d986e340260283a3
Peter Gafert <peter.gafert@tngtech.com>
2020-07-13 16:38:30 +0200

    support concrete array types in generic type signature
    So far types like `Foo<T extends List<SomeType[]>>` were not correctly handled. We now support any arbitrarily nested array types as well, as long as they are concrete array types and not generic array types. Note that by the JLS direct bounds cannot be array types (i.e. `Foo<T extends SomeType[]>` is not a valid signature).
    
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- archunit/src/test/java/com/tngtech/archunit/core/domain/JavaTypeTest.java
+++ archunit/src/test/java/com/tngtech/archunit/core/domain/JavaTypeTest.java
@@ -65,4 +65,22 @@ public class JavaTypeTest {
 
         assertThatType(type.toErasure()).matches(HashMap.class);
     }
+
+    @Test
+    public void erased_type_parameter_of_concrete_array_type_is_array_type() {
+        @SuppressWarnings("unused")
+        class ClassWithBoundTypeParameterWithSingleGenericArrayBound<T extends List<Object[]>, U extends List<String[][]>, V extends List<List<?>[][][]>> {
+        }
+
+        List<JavaTypeVariable> typeParameters = new ClassFileImporter().importClass(ClassWithBoundTypeParameterWithSingleGenericArrayBound.class).getTypeParameters();
+
+        assertThatType(getTypeArgumentOfFirstBound(typeParameters.get(0)).toErasure()).matches(Object[].class);
+        assertThatType(getTypeArgumentOfFirstBound(typeParameters.get(1)).toErasure()).matches(String[][].class);
+        assertThatType(getTypeArgumentOfFirstBound(typeParameters.get(2)).toErasure()).matches(List[][][].class);
+    }
+
+    private static JavaType getTypeArgumentOfFirstBound(JavaTypeVariable typeParameter) {
+        JavaParameterizedType firstBound = (JavaParameterizedType) typeParameter.getBounds().get(0);
+        return firstBound.getActualTypeArguments().get(0);
+    }
 }
